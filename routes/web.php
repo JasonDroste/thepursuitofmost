05d5e968512ad45c2c7c93bd4c370fa2de9d96c5
Jason Droste <jason.droste@hotmail.com>
2024-08-17 20:02:57 -1000

    Added Banner image and masthead

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminProfileController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/dashboard', function () {
