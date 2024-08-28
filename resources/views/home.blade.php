271107067b7d093761deaa39b2a7c7391a5954a0
Jason Droste <jason.droste@hotmail.com>
2024-08-27 20:19:14 -1000

    Mobile adjustments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>The Pursuit of Most</title>
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="{{asset('/img/favicon.io')}}" />
     @vite(['resources/sass/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
     
     <!-- Font Awesome icons (free version)-->
